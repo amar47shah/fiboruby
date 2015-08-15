3e05d61aae788c8587b0d08f6850cec5b2cc699d
Amar Shah <amar@shareprogress.org>
2015-08-15 13:57:47 -0400

    Fix style

--- fibonacci.rb
+++ fibonacci.rb
@@ -6,6 +6,6 @@ class Fibonacci
   def value
     fail ArgumentError, 'index cannot be negative' if @n < 0
     return 1 if @n.zero?
-    (1..@n).reduce([0,1]) { |(pp, p), _| [p, pp + p] }.last
+    (1..@n).reduce([0, 1]) { |(pp, p), _| [p, pp + p] }.last
   end
 end
