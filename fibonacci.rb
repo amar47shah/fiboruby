51df63508472a12f6b65e3014e1a1cc1da362e56
Amar Shah <amar@shareprogress.org>
2015-08-15 14:10:39 -0400

    Raise ArgumentError where it is passed

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,10 +1,10 @@
 class Fibonacci
   def initialize n
+    fail ArgumentError, 'cannot be negative' if n < 0
     @n = n
   end
 
   def value
-    fail ArgumentError, 'index cannot be negative' if @n < 0
     return 1 if @n.zero?
     (1..@n).reduce([0, 1]) { |(pp, p), _| [p, pp + p] }.last
   end
